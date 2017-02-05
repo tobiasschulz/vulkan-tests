8e263039ff604246f645154cb633c4899d13f47e
Tobias Schulz <tobias.schulz186@gmail.com>
2017-02-05 07:32:20 +0100

    ...

---
+++ src/helper/vulkan.cpp
@@ -0,0 +1,26 @@
+//
+// Created by tobias on 05.02.17.
+//
+
+#include "vulkan.h"
+
+VkResult __vkCreateDebugReportCallbackEXT (VkInstance instance, const VkDebugReportCallbackCreateInfoEXT *pCreateInfo,
+                                       const VkAllocationCallbacks *pAllocator, VkDebugReportCallbackEXT *pCallback)
+{
+    auto func = (PFN_vkCreateDebugReportCallbackEXT) vkGetInstanceProcAddr (instance, "vkCreateDebugReportCallbackEXT");
+    if (func != nullptr) {
+        return func (instance, pCreateInfo, pAllocator, pCallback);
+    } else {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+void __vkDestroyDebugReportCallbackEXT (VkInstance instance, VkDebugReportCallbackEXT callback,
+                                    const VkAllocationCallbacks *pAllocator)
+{
+    auto func = (PFN_vkDestroyDebugReportCallbackEXT) vkGetInstanceProcAddr (instance,
+                                                                             "vkDestroyDebugReportCallbackEXT");
+    if (func != nullptr) {
+        func (instance, callback, pAllocator);
+    }
+}
